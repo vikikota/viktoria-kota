7c782e477db2aaeb6eb91cbae5e9bc6e95232c02
vikikota <viktoria.kota@gmail.com>
2024-03-05 21:29:50 +0100

    router bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import './index.css';
 import App from './App';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/hello">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
 );
