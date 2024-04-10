baa9143bb5531989875c1f0a1266f40b1f78911d
vikikota <viktoria.kota@gmail.com>
2024-04-10 09:16:13 +0200

    create qr route & page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 import Layout from './components/Layout';
 import About from './pages/About';
+import QR from './pages/QR';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<Hero />} />
         <Route exact path="/about" element={<About />} />
       </Route>
+        <Route exact path="/qr" element={<QR />} />
     </Routes>
   );
 }
---
+++ src/pages/QR.jsx
@@ -0,0 +1,11 @@
+function QR() {
+    return (
+  <div className="bg-craft flex flex-col bg-bottom">
+      <div className="mx-auto flex flex-col justify-between max-w-[1000px] xl:max-w-[1200px] 2xl:max-w-[1400px] lg:w-4/5 min-h-dvh">
+                  QRFGGGGGGGGGGGGGGGGGGGGGGGGGG
+        </div>
+        </div>
+    )
+}
+
+export default QR;
