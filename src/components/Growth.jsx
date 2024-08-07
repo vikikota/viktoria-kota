07e11f6f3b04ff335ea72b8a215e8835ff9d158f
vikikota <viktoria.kota@gmail.com>
2024-08-07 22:48:35 +0200

    update growth data

--- src/components/Growth.jsx
+++ src/components/Growth.jsx
@@ -13,7 +13,7 @@ function Growth() {
       title: 'FIGMA MasterClass - Web and mobile app UI/UX design',
       description: 'We get to know Figma in depth, enabling us to confidently use it in UI/UX design planning',
       buttonURL: '#',
-      process: 44,
+      process: 47,
     },
     {
       id: 3,
